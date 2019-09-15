a5b4fc8a63540c8fa676c9b69612f1dd440728ea
congenial-language-8324 <kelly.a.manahan@gmail.com>
2019-09-15 17:49:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -22,8 +22,11 @@ end
 
 introduction_with_language_optional("Bob", language)
 
+def introduction_with_language_optional(name, language == "Ruby") 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
 
-
+introduction_with_language_optional("Bob")
 
 
 
